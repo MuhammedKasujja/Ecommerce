5c7546d5515852a9b124c03ac188e767e36ee86f
Muhammed Kasujja <al.kasmud.2@gmail.com>
2023-05-09 09:43:52 +0300

    feat: custom API response structure

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -20,4 +20,47 @@ class Controller extends BaseController
 
         }
     }
+
+    /**
+     * success response method.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function sendResponse($message = "", $success_code = "", $result = []) {
+
+    	$response = ['success' => true, 'data' => []];
+
+        if(!empty($message)) {
+            $response['message'] = $message;
+        }
+
+        if(!empty($success_code)) {
+            $response['code'] = $success_code;
+        }
+
+        if(!empty($result)) {
+            $response['data'] = $result;
+        }
+
+        return response()->json($response, 200);
+    
+    }
+
+    /**
+     * return error response.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     *
+     */
+    public function sendError($error, $error_code = 101, $error_data = [], $response_code = 200) {
+    	//
+        $response = ['success' => false, 'error' => $error , 'error_code' => $error_code];
+
+        if(!empty($error_data)) {
+            $response['data'] = $error_data;
+        }
+
+        return response()->json($response, $response_code);
+    }
+
 }
